5dab483ef060caa6ba46cd24078bce641c638cdd
jpresern <janez.presern@gmail.com>
2015-05-15 15:31:43 +0200

    Reads and outputs info.dat

--- pyrelacs/DataClasses/__init__.py
+++ pyrelacs/DataClasses/__init__.py
@@ -1,11 +1,13 @@
 import re
-from .RelacsFile import SpikeFile, BeatFile, StimuliFile, FICurveFile, RelacsFile, TraceFile, InfoFile
+from .RelacsFile import SpikeFile, BeatFile, StimuliFile, FICurveFile, RelacsFile, TraceFile, read_info_file
 
 
 def load(filename):
-    if re.match(".*stimspikes.*\.dat$", filename) or re.match(".*samallspikes.*\.dat$", filename):
+    if re.match(".*info.*\.dat$", filename):
+        return read_info_file(filename)
+    elif re.match(".*stimspikes.*\.dat$", filename) or re.match(".*samallspikes.*\.dat$", filename):
         return SpikeFile(filename)
-    elif re.match("ficurve-spikes.*\.dat$", filename):
+    elif re.match(".*ficurve-spikes.*\.dat$", filename):
          return SpikeFile(filename, mergetrials=False)
     elif re.match(".*beats-eod.*\.dat$", filename):
          return BeatFile(filename)
@@ -13,8 +15,6 @@ def load(filename):
         return StimuliFile(filename)
     elif re.match(".*ficurves.*\.dat$", filename):
         return FICurveFile(filename)
-    elif re.match(".*info.*\.dat$", filename):
-        return InfoFile(filename)
     elif re.match(".*ficurve-.*\.dat$", filename) or re.match(".*vicurve-.*\.dat$", filename) \
             or re.match(".*transferfunction-data.*\.dat$", filename) \
             or re.match(".*membraneresistance-trace.*\.dat$", filename) \
